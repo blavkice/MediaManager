7f60305e410fde3452ba999151c05e3ac5f75326
blavkice <beppespqr@gmail.com>
2025-02-13 12:40:16 +0100

    created media class

---
+++ Model/Media.cpp
@@ -0,0 +1,38 @@
+#include "Media.h"
+
+Media::Media(const unsigned int id, const QString& title, const QString& short_description, const QString& image_path)
+    : id(id), title(title), shortDescription(short_description), imagePath(image_path) { }
+
+Media::~Media() = default;
+
+unsigned int Media::getId() const {
+    return id;
+}
+
+QString Media::getTitle() const {
+    return title;
+}
+
+QString Media::getShortDescription() const {
+    return shortDescription;
+}
+
+QString Media::getImagePath() const {
+    return imagePath;
+}
+
+void Media::setId(const unsigned int id) {
+    this->id = id;
+}
+
+void Media::setTitle(const QString& title) {
+    this->title = title;
+}
+
+void Media::setShortDescription(const QString& short_description) {
+    this->shortDescription = short_description;
+}
+
+void Media::setImagePath(const QString& image_path) {
+    this->imagePath = image_path;
+}
---
+++ Model/Media.h
@@ -0,0 +1,24 @@
+#ifndef MEDIA_H
+#define MEDIA_H
+#include <QString>
+
+class Media {
+private:
+    unsigned int id;
+    QString title;
+    QString shortDescription;
+    QString imagePath;
+public:
+    Media(unsigned int id, const QString &title, const QString &short_description, const QString &image_path);
+    virtual ~Media() = 0;
+    unsigned int getId() const;
+    QString getTitle() const;
+    QString getShortDescription() const;
+    QString getImagePath() const;
+    void setId(unsigned int id);
+    void setTitle(const QString &title);
+    void setShortDescription(const QString &short_description);
+    void setImagePath(const QString &image_path);
+};
+
+#endif //MEDIA_H
