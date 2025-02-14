e5e7a921e44ee7f933b1223f9dd6bcff024c0d64
blavkice <beppespqr@gmail.com>
2025-02-14 16:00:23 +0100

    main layouts

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -1,19 +1,43 @@
 #include "MainWindow.h"
 
 MainWindow::MainWindow(QWidget* parent) : QMainWindow(parent) {
+    resize(800, 600);
+    // menu bar initialization
     menuBar = new QMenuBar(this);
     setMenuBar(menuBar);
     centralWidget = new QWidget(this);
     setCentralWidget(centralWidget);
     initMenuBar();
+    // initialize layouts: the left vertical one and the main horizontal one on the right
+    hMainViewLayout = new QHBoxLayout(centralWidget);
+    auto vLeftWidget = new QWidget(centralWidget);
+    const auto searchLabel = new QLabel("Search item:", vLeftWidget);
+    vLeftLayout = new QVBoxLayout(vLeftWidget);
+    searchBox = new QLineEdit(vLeftWidget);
+    listView = new QListView(vLeftWidget);
+    gridLayout = new QGridLayout(centralWidget);
+    // add widgets to layouts
+    vLeftLayout->addWidget(searchLabel);
+    vLeftLayout->addWidget(searchBox);
+    vLeftLayout->addWidget(listView);
+    vLeftWidget->setFixedWidth(250);
+    hMainViewLayout->addWidget(vLeftWidget);
+    hMainViewLayout->addItem(new QSpacerItem(20, 0, QSizePolicy::Expanding, QSizePolicy::Minimum));
+    hMainViewLayout->addLayout(gridLayout);
+    centralWidget->setLayout(hMainViewLayout);
+}
+
+void MainWindow::initLayouts() {
+    // TBD
 }
 
 void MainWindow::initMenuBar() {
     actionsMenu = menuBar->addMenu(tr("Actions"));
     // actions
-    exit = new QAction(tr("Exit"), this);
+    exit = new QAction(tr("Exit MediaManager"), this);
+    exit->setMenuRole(QAction::NoRole);
     save = new QAction(tr("Save"), this);
-    actionsMenu->addAction(exit);
     actionsMenu->addAction(save);
+    actionsMenu->addAction(exit);
     connect(exit, &QAction::triggered, this, &MainWindow::close);
 }
--- src/MainWindow.h
+++ src/MainWindow.h
@@ -2,7 +2,14 @@
 #define MAINWINDOW_H
 #include <QMainWindow>
 #include <QMenu>
+// ReSharper disable once CppUnusedIncludeDirective
 #include <QMenuBar>
+#include <QHBoxLayout>
+#include <QVBoxLayout>
+#include <QListView>
+#include <QGridLayout>
+#include <QLineEdit>
+#include <QLabel>
 
 // ReSharper disable once CppClassCanBeFinal
 class MainWindow : public QMainWindow {
@@ -14,6 +21,12 @@ private:
     QAction* exit;
     QAction* save; // TBD real persistence
     void initMenuBar();
+    void initLayouts();
+    QHBoxLayout* hMainViewLayout;
+    QVBoxLayout* vLeftLayout;
+    QLineEdit* searchBox;
+    QListView* listView;
+    QGridLayout* gridLayout;
 public:
     explicit MainWindow(QWidget* parent = nullptr);
 };
