939018eef8fdb33ea03e36d7119e1e978ec5d45c
blavkice <beppespqr@gmail.com>
2025-02-20 17:21:18 +0100

    corrected double poem view

--- src/Model/ViewVisitor.cpp
+++ src/Model/ViewVisitor.cpp
@@ -3,21 +3,21 @@
 ViewVisitor::ViewVisitor(QLayout* layout) : layout(layout) {}
 
 void ViewVisitor::addLabel(const QString& text, const QString& style) const {
-    QLabel* label = new QLabel(text);
+    auto* label = new QLabel(text);
     label->setStyleSheet(style);
     label->setWordWrap(true);
     layout->addWidget(label);
 }
 
 void ViewVisitor::addTextBlock(const QString& text) const {
-    QTextEdit* textEdit = new QTextEdit(text);
+    auto* textEdit = new QTextEdit(text);
     textEdit->setReadOnly(true);
     textEdit->setStyleSheet("background-color: #f5f5f5; border-radius: 8px; padding: 10px;");
     layout->addWidget(textEdit);
 }
 
 void ViewVisitor::visit(Media* media) {
-    addLabel("Title: " + media->getTitle(), "font-size: 24px; font-weight: bold;");
+    addLabel("Title: " + media->getTitle(), "font-size: 30px; font-weight: bold;");
     addLabel("Description: " + media->getShortDescription());
 }
 
@@ -47,7 +47,7 @@ void ViewVisitor::visit(Book* book) {
 void ViewVisitor::visit(Poem* poem) {
     visit(static_cast<Literature*>(poem));
     addLabel("City of Origin: " + poem->getCity());
-    addTextBlock(poem->getLongDescription());
+    //addTextBlock(poem->getLongDescription());
 }
 
 void ViewVisitor::visit(AcademicArticle* academicArticle) {
@@ -59,6 +59,6 @@ void ViewVisitor::visit(AcademicArticle* academicArticle) {
 
 void ViewVisitor::visit(NewspaperArticle* newspaperArticle) {
     visit(static_cast<Articles*>(newspaperArticle));
-    addLabel("Headline: " + newspaperArticle->getHeadline(), "font-size: 20px; font-weight: bold;");
+    addLabel("Headline: " + newspaperArticle->getHeadline(), "font-size: 25px; font-weight: bold;");
     addLabel("Politics Section: " + QString(newspaperArticle->isPolitics() ? "Yes" : "No"));
 }
